e5bf630da624ab127fa58aad46afd770c9c3ba2d
Slezin Ivan <ftsx0xstf@gmail.com>
2023-10-26 17:05:14 +0300

    Step four + gitIgnore node_modules

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .version('0.0.1', '-v, --version', 'output the version number.')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
   .helpOption('-h, --help', 'output usage information')
   .action((filepath1, filepath2) => {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 import getParse from './parsers.js';
+import _ from 'lodash';
 
 const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
 const readFile = (filePath) =>
@@ -16,11 +17,41 @@ const getFileExtension = (filePath) => path.extname(filePath).slice(1);
 // '__fixtures__/file1.json'
 // '__fixtures__/file2.json'
 
+// gendiff __fixtures__/file1.json __fixtures__/file2.json
+
+const findDifferences = (file1, file2) => {
+  const keysFile1 = Object.keys(file1); // console.log(`Ключи первого файла: `, keysFile1);
+  const keysFile2 = Object.keys(file2); // console.log(`Ключи второго файла: `, keysFile2);
+  const getAllUniqKeys = _.union(keysFile1, keysFile2).sort();
+  // console.log('Уникальные ключи: ', getAllUniqKeys);
+
+  const result = getAllUniqKeys.reduce((acc, key) => {
+    if (keysFile1.includes(key) && keysFile2.includes(key)) {
+      if (file1[key] === file2[key]) {
+        acc = `${acc}    ${key}: ${file1[key]}\n`;
+      }
+      if (file1[key] !== file2[key]) {
+        acc = `${acc}  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}\n`;
+      }
+    }
+    if (keysFile1.includes(key) && !keysFile2.includes(key)) {
+      acc = `${acc}  - ${key}: ${file1[key]}\n`;
+    }
+    if (keysFile2.includes(key) && !keysFile1.includes(key)) {
+      acc = `${acc}  + ${key}: ${file2[key]}\n`;
+    }
+
+    return acc;
+  }, '');
+
+  return `{\n${result}}`;
+};
+
 const genDiff = (filepath1, filepath2) => {
   const fileReading1 = readFile(filepath1);
   const fileReading2 = readFile(filepath2);
   const filePars1 = getParse(fileReading1, getFileExtension(filepath1));
   const filePars2 = getParse(fileReading2, getFileExtension(filepath2));
-  return console.log(fileReading1);
+  return findDifferences(filePars1, filePars2);
 };
 export default genDiff;
