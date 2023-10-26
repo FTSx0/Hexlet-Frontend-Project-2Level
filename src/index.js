c001c54a5c52ea35bec5b802012430b1fa8c0f5a
Slezin Ivan <ftsx0xstf@gmail.com>
2023-10-26 23:59:41 +0300

    Update and fix

--- src/index.js
+++ src/index.js
@@ -1,44 +1,35 @@
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
-import getParse from './parsers.js';
 import _ from 'lodash';
+import getParse from './parsers.js';
 
-const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
-const readFile = (filePath) =>
-  fs.readFileSync(getAbsolutePath(filePath), 'utf8');
+const getAbsPath = (filePath) => path.resolve(process.cwd(), filePath);
+const readFile = (filePath) => fs.readFileSync(getAbsPath(filePath), 'utf8');
 
 const getFileExtension = (filePath) => path.extname(filePath).slice(1);
-
-// const test = getAbsolutePath('__fixtures__/file1.json');
-// const test1 = getFileExtension(test);
-// console.log(test1);
-
-// '__fixtures__/file1.json'
-// '__fixtures__/file2.json'
 
 // gendiff __fixtures__/file1.json __fixtures__/file2.json
 
 const findDifferences = (file1, file2) => {
-  const keysFile1 = Object.keys(file1); // console.log(`Ключи первого файла: `, keysFile1);
-  const keysFile2 = Object.keys(file2); // console.log(`Ключи второго файла: `, keysFile2);
+  const keysFile1 = Object.keys(file1);
+  const keysFile2 = Object.keys(file2);
   const getAllUniqKeys = _.union(keysFile1, keysFile2).sort();
-  // console.log('Уникальные ключи: ', getAllUniqKeys);
 
   const result = getAllUniqKeys.reduce((acc, key) => {
     if (keysFile1.includes(key) && keysFile2.includes(key)) {
       if (file1[key] === file2[key]) {
-        acc = `${acc}    ${key}: ${file1[key]}\n`;
+        return `${acc}    ${key}: ${file1[key]}\n`;
       }
       if (file1[key] !== file2[key]) {
-        acc = `${acc}  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}\n`;
+        return `${acc}  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}\n`;
       }
     }
     if (keysFile1.includes(key) && !keysFile2.includes(key)) {
-      acc = `${acc}  - ${key}: ${file1[key]}\n`;
+      return `${acc}  - ${key}: ${file1[key]}\n`;
     }
     if (keysFile2.includes(key) && !keysFile1.includes(key)) {
-      acc = `${acc}  + ${key}: ${file2[key]}\n`;
+      return `${acc}  + ${key}: ${file2[key]}\n`;
     }
 
     return acc;
