c3ec053ac4de756bb811c886478b792e9d853ead
Slezin Ivan <ftsx0xstf@gmail.com>
2023-11-30 20:37:39 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
-import _ from 'lodash';
+import findDifferences from './diff.js';
 import getParse from './parsers.js';
 
-const getAbsPath = (filePath) => path.resolve(process.cwd(), filePath);
-const readFile = (filePath) => fs.readFileSync(getAbsPath(filePath), 'utf8');
+import stylish from './formatters/stylish.js';
+
+const readFile = (filePath) => {
+  const fullPath = path.resolve(process.cwd(), filePath);
+  return fs.readFileSync(fullPath, 'utf8');
+};
+
+// const getAbsPath = (filePath) => path.resolve(process.cwd(), filePath);
+// const readFile = (filePath) => fs.readFileSync(getAbsPath(filePath), 'utf8');
 
 const getFileExtension = (filePath) => path.extname(filePath).slice(1);
 
@@ -13,38 +20,13 @@ const getFileExtension = (filePath) => path.extname(filePath).slice(1);
 // gendiff __fixtures__/file1.yml __fixtures__/file2.yml
 // gendiff __fixtures__/file1.json __fixtures__/file2.yml
 
-const findDifferences = (file1, file2) => {
-  const keysFile1 = Object.keys(file1);
-  const keysFile2 = Object.keys(file2);
-  const getAllUniqKeys = _.union(keysFile1, keysFile2).sort();
+// gendiff __fixtures__/longFile1.json __fixtures__/longFile2.json
 
-  const result = getAllUniqKeys.reduce((acc, key) => {
-    if (keysFile1.includes(key) && keysFile2.includes(key)) {
-      if (file1[key] === file2[key]) {
-        return `${acc}    ${key}: ${file1[key]}\n`;
-      }
-      if (file1[key] !== file2[key]) {
-        return `${acc}  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}\n`;
-      }
-    }
-    if (keysFile1.includes(key) && !keysFile2.includes(key)) {
-      return `${acc}  - ${key}: ${file1[key]}\n`;
-    }
-    if (keysFile2.includes(key) && !keysFile1.includes(key)) {
-      return `${acc}  + ${key}: ${file2[key]}\n`;
-    }
-
-    return acc;
-  }, '');
-
-  return `{\n${result}}`;
-};
-
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const fileReading1 = readFile(filepath1);
   const fileReading2 = readFile(filepath2);
   const filePars1 = getParse(fileReading1, getFileExtension(filepath1));
   const filePars2 = getParse(fileReading2, getFileExtension(filepath2));
-  return findDifferences(filePars1, filePars2);
+  return stylish(findDifferences(filePars1, filePars2, formatName));
 };
 export default genDiff;
