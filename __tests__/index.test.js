b378a9c4c339bec79303b79df86c59ad8c126e4b
Slezin Ivan <ftsx0xstf@gmail.com>
2023-12-05 05:03:05 +0300

    #4-5-6-7-8, рефакторинг тестов

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,67 +5,30 @@ import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const fixturePath = path.join(__dirname, '..', '__fixtures__');
 
-const expectedData0 = fs.readFileSync(
-  `${fixturePath}/expectedData0.txt`,
-  'utf8',
-);
+const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 
-const expectedData1 = fs.readFileSync(
-  `${fixturePath}/expectedData1.txt`,
-  'utf8',
-);
+const readFile = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf8');
 
-const expectedData2 = fs.readFileSync(
-  `${fixturePath}/expectedData2.txt`,
-  'utf8',
-);
+const stylishData = readFile('expected_stylish_Data.txt');
+const plainData = readFile('expected_plain_Data.txt');
+const jsonData = readFile('expected_json_Data.txt');
 
-const expectedData3 = fs.readFileSync(
-  `${fixturePath}/expectedData3.txt`,
-  'utf8',
-);
-
-describe('Тестируем gendiff', () => {
-  test('JSON-Flat-Stylish', () => {
-    const filepath1 = '__fixtures__/file1.json';
-    const filepath2 = '__fixtures__/file2.json';
-    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedData0);
+describe('Тестируем genDiff', () => {
+  test('*.YML', () => {
+    const file1 = getFixturePath('file1.yml');
+    const file2 = getFixturePath('file2.yml');
+    expect(genDiff(file1, file2, 'stylish')).toEqual(stylishData);
+    expect(genDiff(file1, file2, 'plain')).toEqual(plainData);
+    expect(genDiff(file1, file2, 'json')).toEqual(jsonData);
+    expect(genDiff(file1, file2)).toEqual(stylishData);
   });
-  test('YML-Flat-Stylish', () => {
-    const filepath1 = '__fixtures__/file1.yml';
-    const filepath2 = '__fixtures__/file2.yml';
-    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedData0);
-  });
-  test('JSON-Nested-Stylish', () => {
-    const filepath1 = '__fixtures__/longFile1.json';
-    const filepath2 = '__fixtures__/longFile2.json';
-    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedData1);
-  });
-  test('YML-Nested-Stylish', () => {
-    const filepath1 = '__fixtures__/longFile1.yml';
-    const filepath2 = '__fixtures__/longFile2.yml';
-    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedData1);
-  });
-  test('JSON-Nested-Plain', () => {
-    const filepath1 = '__fixtures__/longFile1.json';
-    const filepath2 = '__fixtures__/longFile2.json';
-    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedData2);
-  });
-  test('YML-Nested-Plain', () => {
-    const filepath1 = '__fixtures__/longFile1.yml';
-    const filepath2 = '__fixtures__/longFile2.yml';
-    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedData2);
-  });
-  test('JSON-Stringify', () => {
-    const filepath1 = '__fixtures__/longFile1.json';
-    const filepath2 = '__fixtures__/longFile2.json';
-    expect(genDiff(filepath1, filepath2, 'json')).toEqual(expectedData3);
-  });
-  test('YML-Stringify', () => {
-    const filepath1 = '__fixtures__/longFile1.yml';
-    const filepath2 = '__fixtures__/longFile2.yml';
-    expect(genDiff(filepath1, filepath2, 'json')).toEqual(expectedData3);
+  test('*.JSON', () => {
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.json');
+    expect(genDiff(file1, file2, 'stylish')).toEqual(stylishData);
+    expect(genDiff(file1, file2, 'plain')).toEqual(plainData);
+    expect(genDiff(file1, file2, 'json')).toEqual(jsonData);
+    expect(genDiff(file1, file2)).toEqual(stylishData);
   });
 });
